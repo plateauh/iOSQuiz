a264fc1c4a57fa554538d97b6943ea9b82b6c5b3
plateauh <70834431+plateauh@users.noreply.github.com>
2021-12-06 21:00:29 +0300

    UI and logic done!

--- iOSQuiz2/ViewController.swift
+++ iOSQuiz2/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let questionBank = QuestionBank()
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        if !answerLabel.isHidden {
+            updateUI(hideAnswer: true)
+        }
+        else {
+            updateUI(hideAnswer: false)
+        }
+    }
+    
+    @IBAction func nextButtonPressed(_ sender: UIButton) {
+        questionBank.nextQuestion()
+        updateUI(hideAnswer: true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUI(hideAnswer: true)
     }
 
-
+    func updateUI(hideAnswer: Bool) {
+        let currentQuestion = questionBank.quizBank[questionBank.currentQuestion]
+        questionLabel.text = currentQuestion.0
+        answerLabel.text = currentQuestion.1
+        answerLabel.isHidden = hideAnswer
+    }
 }
 
